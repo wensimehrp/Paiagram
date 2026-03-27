73f9355674c9d6ea435262d5dee8eff315ca7e2c
Jeremy Gao <wensimehrp@gmail.com>
2026-03-27 15:55:14 -0700

    feat: update to egui 0.34.1; replace egui default fonts

---
+++ crates/epaint_default_fonts/src/lib.rs
@@ -0,0 +1,5 @@
+pub const HACK_REGULAR: &[u8] = include_bytes!("../fonts/IosevkaCharonMono-Regular.ttf");
+pub const EMOJI_ICON: &[u8] = include_bytes!("../fonts/emoji-icon-font.ttf");
+// Those are replaced with the subsetted font
+pub const NOTO_EMOJI_REGULAR: &[u8] = HACK_REGULAR;
+pub const UBUNTU_LIGHT: &[u8] = HACK_REGULAR;
