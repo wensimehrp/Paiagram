3af29fea49bbba265f6f37274268c728cd129ca9
Jeremy Gao <wensimehrp@gmail.com>
2026-08-29 21:19:02 -0700

    feat: implement CanvasLength

--- crates/paiagram-core/src/units.rs
+++ crates/paiagram-core/src/units.rs
@@ -5,6 +5,37 @@ pub mod time;
 
 pub use coordinates::*;
 pub use distance::*;
+use serde::{Deserialize, Serialize};
 
 /// The canvas' length in millimetres
-pub struct CanvasLength(f64);
+#[derive(Serialize, Deserialize, Clone, Copy, Debug, PartialEq)]
+pub struct CanvasLength(pub f64);
+
+impl CanvasLength {
+    pub fn from_mm(v: f64) -> Self {
+        CanvasLength(v)
+    }
+    pub fn from_cm(v: f64) -> Self {
+        CanvasLength(v * 10.0)
+    }
+    pub fn from_in(v: f64) -> Self {
+        CanvasLength(v * 25.4)
+    }
+    /// Uses postscript points
+    pub fn from_pts(v: f64) -> Self {
+        CanvasLength(v * 25.4 / 72.0)
+    }
+    pub fn to_mm(&self) -> f64 {
+        self.0
+    }
+    pub fn to_cm(&self) -> f64 {
+        self.0 / 10.0
+    }
+    pub fn to_in(&self) -> f64 {
+        self.0 / 25.4
+    }
+    /// Uses postscript points
+    pub fn to_pts(&self) -> f64 {
+        self.0 * 72.0 / 25.4
+    }
+}
