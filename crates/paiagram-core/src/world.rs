8f75b1b54ef260b8f7b455890350a66d7df75159
Jeremy Gao <wensimehrp@gmail.com>
2026-09-24 19:46:42 -0600

    refactor: remove unused code in world

--- crates/paiagram-core/src/world.rs
+++ crates/paiagram-core/src/world.rs
@@ -7,24 +7,8 @@ impl WorldSnapshot {
         for diff in self.trips.diff(&new.trips) {}
         for diff in self.vehicles.diff(&new.vehicles) {}
         for diff in self.stations.diff(&new.stations) {}
-        for diff in self.intervals.diff(&new.intervals) {}
         for diff in self.service_classes.diff(&new.service_classes) {}
         for diff in self.routes.diff(&new.routes) {}
-        for diff in self.nodes.diff(&new.nodes) {}
         todo!()
-    }
-}
-
-fn merge(old: WorldSnapshot, diff: WorldSnapshot) -> Result<WorldSnapshot, String> {
-    todo!()
-}
-
-// app loop
-fn main_loop() {
-    let mut snap = WorldSnapshot::default();
-    loop {
-        let mut write_snap = snap.clone();
-
-        snap = merge(snap, write_snap).unwrap();
     }
 }
